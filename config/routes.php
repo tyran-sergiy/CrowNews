87150ef58c48c6c3994ff593b65e27c69a697a87
Sergiy Tyran <turan.sergiy@gmail.com>
2016-05-30 13:05:11 +0300

    routes

---
+++ config/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+    "search"=>"site/search",
+    
+    'news/page/([0-9]+)'=>'news/archive/$1',
+    'news/([0-9]+)'=>'news/singleNews/$1',
+    
+    
+    ""=>"site/index"
+    
+    
+);
