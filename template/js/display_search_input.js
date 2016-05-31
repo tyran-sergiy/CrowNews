d3e2b69cfdfdd84a1ac2fca14852a0e21c7b304a
Sergiy Tyran <turan.sergiy@gmail.com>
2016-05-31 17:24:27 +0300

    Delete useless js

--- template/js/display_search_input.js
+++
@@ -1,6 +0,0 @@
-var displaySearch = function(url){
-                $('.search-wrapper').hide();
-               if( trim( url,'/' ) == 'search' ){
-                            $('.search-wrapper').show();
-           }
-           };
--- template/js/trim.js
+++
@@ -1,6 +0,0 @@
-          function trim( str, charlist ) {	
-
-	charlist = !charlist ? ' \s\xA0' : charlist.replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '\$1');
-	var re = new RegExp('^[' + charlist + ']+|[' + charlist + ']+$', 'g');
-	return str.replace(re, '');
-}
